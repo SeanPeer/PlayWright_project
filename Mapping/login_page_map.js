67db4e41fd5ab1199c51a0b0b85e547fb3debb51
seanp <seanpeer@gmail.com>
2024-09-14 17:17:38 +0300

    First

---
+++ Mapping/login_page_map.js
@@ -0,0 +1,10 @@
+
+
+export const login_screen = {
+    username:'#username',
+    password: '#password',
+    user_checlbox: "//div[@class='form-check-inline']//label[1]//span[2]",
+    agree_checkbox: '#terms',
+    sign_button: '#signInBtn',
+    error_message: "[style*='block']"
+}
---
+++ tests/UiBasicTest.spec.js
@@ -0,0 +1,40 @@
+import {login_screen} from "../Mapping/login_page_map"
+
+const {test, expect} = require('@playwright/test');
+const { assert, time } = require('console');
+
+function sleep(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+
+
+test('Browser context initialization', async ({browser}) => {
+
+    const context = await browser.newContext(); //inside the braces i can inject cookies
+    const page = await context.newPage();
+    
+    await page.goto("https://google.com/")
+    const expected_name = 'Google'
+    const actual_name = await page.title()
+    
+    await expect(page).toHaveTitle(expected_name)
+
+} ); 
+
+test.only('First Test', async ({browser, page}) => {
+
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+    await page.locator(login_screen.username).fill("Sean")
+    await page.locator(login_screen.password).fill("123456")
+    await page.locator(login_screen.user_checlbox).click()
+    await page.locator(login_screen.agree_checkbox).click()
+    await page.locator(login_screen.sign_button).click()
+    const expected_error = 'Incorrect username/password.'
+    // const actual_error = await page.locator(login_screen.error_message).textContent()
+
+    await expect(page.locator(login_screen.error_message)).toContainText(expected_error)
+
+
+});
+
+    
